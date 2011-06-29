c0149be19bbb8a2c22d9308af04b39d95b8881ae
Peter Philipp <peter.philipp@cando-image.com>
2011-06-29 12:27:19 +0200

    Play with the UI

--- commerce_currency.admin.js
+++ commerce_currency.admin.js
@@ -7,9 +7,19 @@
       }
       var example = '--';
       if (Number(rate)) {
-        example = rate * 1;
+        example = rate * $('#edit-demo-amount').val();
       }
-      $(this).parent().find('.amount').html(example);
+      $(this).parent().find('.demo-amount-converted').html(example);
+    });
+    $('#edit-demo-amount').keyup(function(){
+      if ($(this).val().search(',')) {
+        $(this).val($(this).val().replace(',', '.'));
+      }
+      if (!Number($(this).val())) {
+        $(this).val(100);
+      }
+      $('.demo-amount').html($(this).val());
+      $('.conversion-rates input.conversion-rate').trigger('keyup');
     });
   });
 })(jQuery);
